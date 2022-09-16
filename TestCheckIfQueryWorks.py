fed56a182eb5f7b76f35efed05298a4e6d0db79c
manjunath krishnappa <manjunath_krishnappa@dell.com>
2022-09-16 02:41:12 +0000

    working TestCheckIfQueryWorks.py

--- TestCheckIfQueryWorks.py
+++ TestCheckIfQueryWorks.py
@@ -9,21 +9,25 @@ from Utils import random_number
 
 if __name__ == '__main__':
 
-    for _ in range(1, 10):
+    for _ in range(1, 1000):
         client = None
 
         try:
             if client is None:
                 client = pymongo.MongoClient(MONGO_URL)
+            #print("MONGO_URL     : " + MONGO_URL)    
+            #print("MONGO_DB_NAME : " + MONGO_DB_NAME)
+            #print("COLL_NAME1    : " + COLL_NAME1)
 
-            client.get_database(MONGO_DB_NAME)
-            db = client.content_db
+            db = client.get_database(MONGO_DB_NAME)
+            coll_names = db.list_collection_names()
+            print(coll_names)
             content_file = db.get_collection(COLL_NAME1)
-            user = 'admin-tenant-' + str(random_number(1, 1000))
-            find_one_result = content_file.find_one({'createdBy': user})
+            user = 'admin-tenant-' + str(_)
+            find_one_result = db.content_file.find_one({'createdBy': user})
 
             if find_one_result is not None:
-                log.info(find_one_result['name'] + " \t " + str(find_one_result['size']))
+                log.info(find_one_result['name'] + " \t " + str(find_one_result['createdBy']))
             else:
                 log.info(" Query returned None for thread {} : and createdBy user : {}".
                          format(threading.current_thread().name, user)
