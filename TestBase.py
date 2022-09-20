976453f10b9f8e9294698ab0ee2608c387c53b58
Manjunath Krishnappa <manjunath_krishnappa@dell.com>
2022-09-20 11:38:05 +0530

    adding new cluster url

--- TestBase.py
+++ TestBase.py
@@ -2,10 +2,10 @@
 #MONGO_URL_MONGO_ATLAS_MANJU = "mongodb+srv://manju:manju@cluster0.wwio5sj.mongodb.net/?retryWrites=true&w=majority"
 #MONGO_URL = "mongodb+srv://manju:manju@cluster0.wwio5sj.mongodb.net/?retryWrites=true&w=majority"
 
-MONGO_URL = "mongodb+srv://manjunath_krishnappa:OX1xjwYRNAzzhJnC@hlx-wu2-nprd-mongo-db-pl-2.hq85i.mongodb.net/?retryWrites=true&w=majority"
+#MONGO_URL = "mongodb+srv://manjunath_krishnappa:OX1xjwYRNAzzhJnC@hlx-wu2-nprd-mongo-db-pl-2.hq85i.mongodb.net/?retryWrites=true&w=majority"
+#MONGO_DB_NAME = "contentmgmt_perf_reg_db"
+#COLL_NAME1 = "content_file"
+
+MONGO_URL = "mongodb+srv://manjunath_krishnappa:OX1xjwYRNAzzhJnC@hlx-wu2-perf-mongo-db-pl-2.hq85i.mongodb.net/?retryWrites=true&w=majority"
 MONGO_DB_NAME = "contentmgmt_perf_reg_db"
 COLL_NAME1 = "content_file"
-
-#MONGO_URL = "mongodb+srv://manjunath_krishnappa:OX1xjwYRNAzzhJnC@hlx-wu2-perf-mongo-db-pl-2.hq85i.mongodb.net/?retryWrites=true&w=majority"
-#MONGO_DB_NAME = "content_db"
-#COLL_NAME1 = "content_file"
